737adbc061b20d1745b268335a140b9a34d16143
wowinn <gedouju@163.com>
2024-05-14 12:50:00 +0800

    注册接口参数校验与异常处理

--- src/main/java/com/yangzhou/controller/UserController.java
+++ src/main/java/com/yangzhou/controller/UserController.java
@@ -3,34 +3,34 @@ package com.yangzhou.controller;
 import com.yangzhou.pojo.Result;
 import com.yangzhou.pojo.User;
 import com.yangzhou.service.UserService;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@Validated
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @PostMapping("/register")
-    public Result register(String username, String password) {
-        if (username != null && username.length() >= 5 && username.length() <= 16 &&
-                password != null && password.length() >= 5 && password.length() <= 16) {
-            //查询用户
-            User u = userService.findByUserName(username);
-            if (u == null) {
-                //注册
-                userService.register(username, password);
-                return Result.success();
-            } else {
-                //占用
-                return Result.error("用户名已被占用");
-            }
+    public Result register(@Pattern(regexp = "^\\S${5,16}") String username, @Pattern(regexp = "^\\S${5,16}") String password) {
+
+        //查询用户
+        User u = userService.findByUserName(username);
+        if (u == null) {
+            //注册
+            userService.register(username, password);
+            return Result.success();
         } else {
-            return Result.error("参数不合法");
+            //占用
+            return Result.error("用户名已被占用");
         }
+
     }
 }
