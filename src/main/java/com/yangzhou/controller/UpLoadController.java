d97fc430da3772b0c911828a1650b8fad37ba651
wowinn <gedouju@163.com>
2024-05-17 18:35:26 +0800

    项目部署

--- src/main/java/com/yangzhou/controller/UpLoadController.java
+++ src/main/java/com/yangzhou/controller/UpLoadController.java
@@ -15,7 +15,7 @@ public class UpLoadController {
     @PostMapping("/upload")
     public Result<String> upload(MultipartFile file) throws Exception {
         String originalFilename = file.getOriginalFilename();
-        String filename = UUID.randomUUID().toString()+originalFilename.substring(originalFilename.lastIndexOf("."));
+        String filename = UUID.randomUUID().toString() + originalFilename.substring(originalFilename.lastIndexOf("."));
         String url = AliOssUtil.upLoadFile(filename, file.getInputStream());
         return Result.success(url);
     }
